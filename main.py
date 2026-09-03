fa4de614e4a2822e4222b29e56e8cb4db73bb7f5
DiegoSCC <diego.santino03@gmail.com>
2026-09-03 16:15:23 +0000

    Initial commit: created main.py, constants.py and a logger file

---
+++ main.py
@@ -0,0 +1,11 @@
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+import pygame
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print((f"Screen width: {SCREEN_WIDTH}"))
+
+
+if __name__ == "__main__":
+    main()
